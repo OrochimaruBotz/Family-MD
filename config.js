a36142da488487126d019cca93ec91845c4ffb46
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-08-03 23:28:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 888-0494-7282'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
@@ -63,7 +63,7 @@ global.author = data.owner;
 
 //=========== Requirements ==========//  
         global.colong1 = 'Ciss 📸'
-        global.colong2 = 'ʙy ᴀɢᴜᴢ ꜰᴀᴍɪʟɪᴀ'
+        global.colong2 = 'ʙy Yamato'
 global.img = pickRandom(waifu)
 global.fla = pickRandom(flaaa)
 
