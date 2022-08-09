504a7a04e4fc8c9433244488c4b2d9f85a1acf99
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-08-09 16:19:01 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,14 +2,8 @@ const qrku = "https://i.ibb.co/MnbqZ0N/qr-Aguz-Familia-07-06-22-1654614928.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
-╰────
-╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281320170984
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
+│ • Telkomsel [088804947282]
+│ • Dana  [088804947282]
 ╰────
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
